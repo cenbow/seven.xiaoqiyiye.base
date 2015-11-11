0065d179c46dbdefad7295e58ab28e726ade1c24
455402494@qq.com <linya111111@126.com>
2015-11-11 08:18:02 +0800

    qw

---
+++ seven.springmvc/src/main/java/seven/xiaoqiyiye/mvc/ex01/HelloController.java
@@ -0,0 +1,17 @@
+package seven.xiaoqiyiye.mvc.ex01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+	model.addAttribute("name", "linya");
+	return "/hello";
+    }
+    
+    
+}
